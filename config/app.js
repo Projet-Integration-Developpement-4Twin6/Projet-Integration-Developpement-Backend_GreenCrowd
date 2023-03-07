28922ff070798d561531523fc8ccec5686d58180
charrada <61417722+charrada@users.noreply.github.com>
2023-03-07 22:32:07 +0100

    Login
    First commit

--- config/app.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  appKey: process.env.APP_key,
-  tokenExpiresIn: 3600
-};
--- exceptions/invalid-credential-exception.js
+++
@@ -1,9 +0,0 @@
-const BaseException=require('./base-exception')
-
-class InvalidCreationException extends BaseException{
- constructor(message,status=403){
-super(message,status)
- }
-}
-
-module.exports=InvalidCreationException
